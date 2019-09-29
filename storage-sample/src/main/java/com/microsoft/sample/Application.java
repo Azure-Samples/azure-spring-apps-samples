b3d02ac147095a3850feb90a22fed12fd0041ec0
Kevin Zhao <kvn.zhl@gmail.com>
2019-09-29 14:17:15 +0800

    Update Application.java

--- storage-sample/src/main/java/com/microsoft/sample/Application.java
+++ storage-sample/src/main/java/com/microsoft/sample/Application.java
@@ -1,8 +1,10 @@
-package com.microsoft.sample;/*
+/*
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for
  * license information.
  */
+
+package com.microsoft.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
