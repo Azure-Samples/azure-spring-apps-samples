8d1355c55c934ab9dad5e2cb1b42576f10e1f0ce
Raphael Yu <xianyu@microsoft.com>
2019-09-30 15:30:01 +0800

    Change the redis sample.

--- service-binding-redis/src/main/java/com/microsoft/azure/ServiceBindingRedisApplication.java
+++ service-binding-redis/src/main/java/com/microsoft/azure/ServiceBindingRedisApplication.java
@@ -5,15 +5,37 @@
  */
 package com.microsoft.azure;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ServiceBindingRedisApplication {
+@Service
+public class ServiceBindingRedisApplication implements CommandLineRunner {
+
+	@Autowired
+	StringRedisTemplate redis;
+
+	@Autowired
+	RedisConnectionFactory factory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceBindingRedisApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		factory.getConnection().flushAll();
+		Stream.of("Alice", "Bob", "Smith")
+				.forEach(name -> redis.opsForValue().set(UUID.randomUUID().toString(), name));
 	}
 }
