e051a3baea3466fbe1558327323057c6c2b9ced3
Raphael Yu <yyddxxmm@gmail.com>
2019-10-08 20:03:28 +0800

    Fix.

--- service-binding-cosmosdb-sql/src/main/java/com/microsoft/azure/MainController.java
+++ service-binding-cosmosdb-sql/src/main/java/com/microsoft/azure/MainController.java
@@ -6,17 +6,16 @@
 package com.microsoft.azure;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class MainController {
 	@Autowired
 	private UserRepository userRepository;
 
 	@GetMapping(path="/all")
-	public @ResponseBody Iterable<User> getAllUsers() {
+	public Iterable<User> getAllUsers() {
 		return userRepository.findAll();
 	}
 
--- service-binding-mysql/src/main/java/com/microsoft/azure/MainController.java
+++ service-binding-mysql/src/main/java/com/microsoft/azure/MainController.java
@@ -6,13 +6,13 @@
 package com.microsoft.azure;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@RestController
 public class MainController {
 	@Autowired
 	private UserRepository userRepository;
