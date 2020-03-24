63db73d33342f06cd9085352d3184a4cadbaad96
Junyi Yi <junyi@microsoft.com>
2020-03-24 01:53:42 -0700

    Add config server sample project.

---
+++ steeltoe-sample/src/ConfigServer/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.SpringCloud.Client;
+using Microsoft.Extensions.Hosting;
+
+using Steeltoe.Extensions.Configuration.ConfigServer;
+
+namespace Microsoft.Azure.SpringCloud.Sample.ConfigServer
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .AddConfigServer()
+                .UseAzureSpringCloudService();
+    }
+}
