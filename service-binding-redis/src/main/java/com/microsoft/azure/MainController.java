cb06a116bdb6097bea3d2163fb37da686c505b23
Menghua Xiao <arieshout@gmail.com>
2020-06-10 16:19:48 +0800

    Remove dangerous code in service-binding-redis that clears all the Redis keys (#29)

--- service-binding-redis/src/main/java/com/microsoft/azure/MainController.java
+++ service-binding-redis/src/main/java/com/microsoft/azure/MainController.java
@@ -5,7 +5,6 @@
  */
 package com.microsoft.azure;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +14,18 @@ import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
-    @Autowired
-    StringRedisTemplate redis;
+    private final StringRedisTemplate redis;
+
+    public MainController(StringRedisTemplate redis) {
+        this.redis = redis;
+    }
 
     @GetMapping("/all")
     public List<String> all() {
-        return redis.keys("*").stream().map(redis.opsForValue()::get).collect(Collectors.toList());
+        return redis.keys("*").stream().sorted().collect(Collectors.toList());
     }
 
-    @GetMapping(path="/hello")
+    @GetMapping(path = "/hello")
     public String hello() {
         return "hello!";
     }
