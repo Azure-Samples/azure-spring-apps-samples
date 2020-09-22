e79f194b034463f00a576705a384a292e9861fb3
Junyi Yi <junyi@microsoft.com>
2020-09-21 21:38:37 -0700

    Upgrade to public preview AzureSpringCloud Steeltoe Client library.

--- steeltoe-sample/src/solar-system-weather/Program.cs
+++ steeltoe-sample/src/solar-system-weather/Program.cs
@@ -1,10 +1,6 @@
-using System;
-
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Hosting;
-
 using Microsoft.Azure.SpringCloud.Client;
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Azure.SpringCloud.Sample.SolarSystemWeather
 {
@@ -15,6 +11,30 @@ namespace Microsoft.Azure.SpringCloud.Sample.SolarSystemWeather
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Configure steeltoe and Azure Spring Cloud.
+        ///
+        /// Please ensure you are using the correct version of Microsoft.Azure.SpringCloud.Client:
+        /// <list type="bullet">
+        ///     <item><term>1.0.0-preview.1</term> for Steeltoe library 2.4.4</item>
+        ///     <item><term>2.0.0-preview.1</term> for Steeltoe library 3.0.0</item>
+        /// </list>
+        ///
+        /// Please also notice the sequence of <c>.AddConfigServer()</c> and <c>.UseAzureSpringCloudService()</c>.
+        /// If you do not follow the correct sequence mentioned below, your app will not run in Azure.
+        ///
+        /// For different versions of Microsoft.Azure.SpringCloud.Client:
+        /// <list type="bullet">
+        ///     <item>
+        ///         <term>1.0.0-preview.1</term>
+        ///         <c>.UseAzureSpringCloudService()</c> must be added AFTER <c>.AddConfigServer()</c>
+        ///     </item>
+        ///     <item>
+        ///         <term>2.0.0-preview.1</term>
+        ///         <c>.UseAzureSpringCloudService()</c> must BEFORE any other Steeltoe configurations
+        ///     </item>
+        /// </list>
+        /// </summary>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
