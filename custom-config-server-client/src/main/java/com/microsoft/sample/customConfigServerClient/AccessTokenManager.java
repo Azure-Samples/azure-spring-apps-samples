21de8e282fcfa56fa8dd7e30241935e8fdb96f24
Xiaoyun Ding <xiading@microsoft.com>
2023-12-04 21:42:22 +0800

    add scope when get token

--- custom-config-server-client/src/main/java/com/microsoft/sample/customConfigServerClient/AccessTokenManager.java
+++ custom-config-server-client/src/main/java/com/microsoft/sample/customConfigServerClient/AccessTokenManager.java
@@ -6,7 +6,10 @@
 
 package com.microsoft.sample.customConfigServerClient;
 
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.identity.ClientSecretCredential;
 import com.microsoft.azure.AzureEnvironment;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import com.microsoft.azure.credentials.ApplicationTokenCredentials;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,8 +26,15 @@ public class AccessTokenManager {
             String tenantId = prop.getProperty("access.token.tenantId");
             String secret = prop.getProperty("access.token.secret");
             String clientId = prop.getProperty("access.token.clientId");
-            credentials = new ApplicationTokenCredentials(
-                clientId, tenantId, secret, AzureEnvironment.AZURE);
+            ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+                .clientId(clientId)
+                .clientSecret(secret)
+                .tenantId(tenantId)
+                .authorityHost(AzureEnvironment.AZURE.activeDirectoryEndpoint())
+                .build();
+
+            TokenRequestContext tokenRequest = new TokenRequestContext().addScopes("https://management.core.windows.net//.default");
+            String token = credential.getToken(tokenRequest).block().getToken();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
