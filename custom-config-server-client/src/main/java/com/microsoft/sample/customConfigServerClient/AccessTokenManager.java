b35dea533537d5c0855e8e79af8bd07e7fb9032e
york <yorkzhang@microsoft.com>
2023-12-07 14:05:41 +0800

    adding scope to fix the start up issue

--- custom-config-server-client/src/main/java/com/microsoft/sample/customConfigServerClient/AccessTokenManager.java
+++ custom-config-server-client/src/main/java/com/microsoft/sample/customConfigServerClient/AccessTokenManager.java
@@ -6,35 +6,38 @@
 
 package com.microsoft.sample.customConfigServerClient;
 
+import com.azure.core.credential.TokenRequestContext;
+import com.azure.identity.ClientSecretCredential;
 import com.microsoft.azure.AzureEnvironment;
-import com.microsoft.azure.credentials.ApplicationTokenCredentials;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import com.azure.identity.ClientSecretCredentialBuilder;
 
 public class AccessTokenManager {
-
+    private static String token;
     static {
         Properties prop = new Properties();
         InputStream in = AccessTokenManager.class.getResourceAsStream("/application.properties");
         try {
             prop.load(in);
-            tokenClientId = prop.getProperty("access.token.clientId");
             String tenantId = prop.getProperty("access.token.tenantId");
             String secret = prop.getProperty("access.token.secret");
             String clientId = prop.getProperty("access.token.clientId");
-            credentials = new ApplicationTokenCredentials(
-                clientId, tenantId, secret, AzureEnvironment.AZURE);
+            ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+                    .clientId(clientId)
+                    .clientSecret(secret)
+                    .tenantId(tenantId)
+                    .authorityHost(AzureEnvironment.AZURE.activeDirectoryEndpoint())
+                    .build();
+            TokenRequestContext tokenRequest = new TokenRequestContext().addScopes("https://management.core.windows.net//.default");
+            token = credential.getToken(tokenRequest).block().getToken();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    private static String tokenClientId;
-
-    private static ApplicationTokenCredentials credentials;
-
     public static String getToken() throws IOException {
-        return credentials.getToken(tokenClientId);
+        return token;
     }
 }
