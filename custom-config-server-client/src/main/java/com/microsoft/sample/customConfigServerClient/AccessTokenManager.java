d854c93218a64d8413d87bbfaa3717d593950cfe
yorkzhang <yorkzhangsir@gmail.com>
2023-12-07 21:32:56 +0800

    adding scope
    adding scope

--- custom-config-server-client/src/main/java/com/microsoft/sample/customConfigServerClient/AccessTokenManager.java
+++ custom-config-server-client/src/main/java/com/microsoft/sample/customConfigServerClient/AccessTokenManager.java
@@ -9,42 +9,35 @@ package com.microsoft.sample.customConfigServerClient;
 import com.azure.core.credential.TokenRequestContext;
 import com.azure.identity.ClientSecretCredential;
 import com.microsoft.azure.AzureEnvironment;
-import com.azure.identity.ClientSecretCredentialBuilder;
-import com.microsoft.azure.credentials.ApplicationTokenCredentials;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import com.azure.identity.ClientSecretCredentialBuilder;
 
 public class AccessTokenManager {
-
+    private static String token;
     static {
         Properties prop = new Properties();
         InputStream in = AccessTokenManager.class.getResourceAsStream("/application.properties");
         try {
             prop.load(in);
-            tokenClientId = prop.getProperty("access.token.clientId");
             String tenantId = prop.getProperty("access.token.tenantId");
             String secret = prop.getProperty("access.token.secret");
             String clientId = prop.getProperty("access.token.clientId");
             ClientSecretCredential credential = new ClientSecretCredentialBuilder()
-                .clientId(clientId)
-                .clientSecret(secret)
-                .tenantId(tenantId)
-                .authorityHost(AzureEnvironment.AZURE.activeDirectoryEndpoint())
-                .build();
-
+                    .clientId(clientId)
+                    .clientSecret(secret)
+                    .tenantId(tenantId)
+                    .authorityHost(AzureEnvironment.AZURE.activeDirectoryEndpoint())
+                    .build();
             TokenRequestContext tokenRequest = new TokenRequestContext().addScopes("https://management.core.windows.net//.default");
-            String token = credential.getToken(tokenRequest).block().getToken();
+            token = credential.getToken(tokenRequest).block().getToken();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    private static String tokenClientId;
-
-    private static ApplicationTokenCredentials credentials;
-
     public static String getToken() throws IOException {
-        return credentials.getToken(tokenClientId);
+        return token;
     }
 }
