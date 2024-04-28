a882ac87039e610ec5f6856284c451aef2462ef5
Dingmeng Xue <dixue@microsoft.com>
2024-04-28 17:23:05 +0800

    update

--- job-samples/football-billboard/src/main/java/com/microsoft/sample/football/BillboardApplication.java
+++ job-samples/football-billboard/src/main/java/com/microsoft/sample/football/BillboardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient(autoRegister = true)
+@EnableDiscoveryClient
 public class BillboardApplication {
 
     public static void main(String[] args) {
--- job-samples/football/src/main/java/com/microsoft/sample/FootballJobApplication.java
+++ job-samples/football/src/main/java/com/microsoft/sample/FootballJobApplication.java
@@ -9,49 +9,57 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.batch.samples.football.FootballJobConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 
-public class FootballJobApplication {
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FootballJobApplication implements CommandLineRunner {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(FootballJobApplication.class);
-    
-    @Autowired
-    private DiscoveryClient discoveryClient;
-    
-    public static void main(String[] args) throws Exception {
+	private final static Logger LOGGER = LoggerFactory.getLogger(FootballJobApplication.class);
 
-        try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(FootballJobConfiguration.class)) {
-            JobLauncher jobLauncher = context.getBean(JobLauncher.class);
-            Job job = context.getBean(Job.class);
-            DataSource ds = context.getBean(DataSource.class);
-            int count = JdbcClient.create(new JdbcTemplate(ds))
-                    .sql("SELECT COUNT(0) FROM PLAYER_SUMMARY")
-                    .query(Integer.class)
-                    .single();
-            LOGGER.info("There is {} player summary before job execution");
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
-            jobLauncher.run(job, new JobParameters());
-            
-            count = JdbcClient.create(new JdbcTemplate(ds))
-                    .sql("SELECT COUNT(0) FROM PLAYER_SUMMARY")
-                    .query(Integer.class)
-                    .single();
-            LOGGER.info("There is {} player summary after job execution", count);
-            
-            ResultReport result = new ResultReport();
-            result.setLastExecuted(new Date());
-            result.setSummaryCount(count);
-            
-            RestTemplate restTemplate = new RestTemplate();
-            restTemplate.postForEntity(null, result, ResultReport.class);            
-        }
-    }
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	private Job job;
+
+	@Autowired
+	private DataSource ds;
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FootballJobApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		int count = JdbcClient.create(new JdbcTemplate(ds)).sql("SELECT COUNT(0) FROM PLAYER_SUMMARY")
+				.query(Integer.class).single();
+		LOGGER.info("There is {} player summary before job execution");
+
+		jobLauncher.run(job, new JobParameters());
+
+		count = JdbcClient.create(new JdbcTemplate(ds)).sql("SELECT COUNT(0) FROM PLAYER_SUMMARY").query(Integer.class)
+				.single();
+		LOGGER.info("There is {} player summary after job execution", count);
+
+		ResultReport result = new ResultReport();
+		result.setLastExecuted(new Date());
+		result.setSummaryCount(count);
+
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.postForEntity(null, result, ResultReport.class);
+	}
 
 }
