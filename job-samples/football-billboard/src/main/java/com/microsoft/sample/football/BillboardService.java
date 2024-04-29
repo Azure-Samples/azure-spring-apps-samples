378e8bcbb824676ace8e104cbab7740822c0a7ee
Dingmeng Xue <dixue@microsoft.com>
2024-04-29 22:23:03 +0800

    update

--- job-samples/football-billboard/src/main/java/com/microsoft/sample/football/BillboardService.java
+++ job-samples/football-billboard/src/main/java/com/microsoft/sample/football/BillboardService.java
@@ -5,25 +5,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class BillboardService {
 
-	private ResultReport resultReport;
+    private ResultReport resultReport;
 
-	/**
-	 * @return the resultReport
-	 */
-	public ResultReport getResultReport() {
-		return resultReport;
-	}
+    /**
+     * @return the resultReport
+     */
+    public ResultReport getResultReport() {
+        return resultReport;
+    }
 
-	/**
-	 * @param resultReport the resultReport to set
-	 */
-	public void setResultReport(ResultReport resultReport) {
-		if (resultReport == null) {
-			this.resultReport = resultReport;
-		} else {
-			this.resultReport.setSummaryCount(this.resultReport.getSummaryCount() + resultReport.getSummaryCount());
-			this.resultReport.setLastExecuted(resultReport.getLastExecuted());
-		}
-	}
+    /**
+     * @param resultReport the resultReport to set
+     */
+    public void setResultReport(ResultReport resultReport) {
+        if(resultReport == null) {
+            return;
+        }
+        
+        if (this.resultReport == null) {
+            this.resultReport = resultReport;
+        } else {
+            this.resultReport.setSummaryCount(this.resultReport.getSummaryCount() + resultReport.getSummaryCount());
+            this.resultReport.setLastExecuted(resultReport.getLastExecuted());
+        }
+    }
 
 }
