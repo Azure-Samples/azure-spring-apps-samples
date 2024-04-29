258f3218abf7a31d2e7d3a525df4c8ef61ffb29a
Dingmeng Xue <dixue@microsoft.com>
2024-04-29 22:30:52 +0800

    update

--- job-samples/football-billboard/src/main/java/com/microsoft/sample/football/BillboardService.java
+++ job-samples/football-billboard/src/main/java/com/microsoft/sample/football/BillboardService.java
@@ -14,27 +14,14 @@ public class BillboardService {
         return resultReport;
     }
 
-<<<<<<< HEAD
-	/**
-	 * @param resultReport the resultReport to set
-	 */
-	public void setResultReport(ResultReport resultReport) {
-		if (this.resultReport == null) {
-			this.resultReport = resultReport;
-		} else {
-			this.resultReport.setSummaryCount(this.resultReport.getSummaryCount() + resultReport.getSummaryCount());
-			this.resultReport.setLastExecuted(resultReport.getLastExecuted());
-		}
-	}
-=======
     /**
      * @param resultReport the resultReport to set
      */
     public void setResultReport(ResultReport resultReport) {
-        if(resultReport == null) {
+        if (resultReport == null) {
             return;
         }
-        
+
         if (this.resultReport == null) {
             this.resultReport = resultReport;
         } else {
@@ -42,6 +29,5 @@ public class BillboardService {
             this.resultReport.setLastExecuted(resultReport.getLastExecuted());
         }
     }
->>>>>>> 378e8bcbb824676ace8e104cbab7740822c0a7ee
 
 }
