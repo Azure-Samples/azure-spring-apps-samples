fa7f0ffea19fd39e85830434272ee0a1ab460063
Dingmeng Xue <dixue@microsoft.com>
2024-08-09 10:54:55 +0800

    Add java 17 sample

---
+++ java-17-sample/src/main/java/com/azure/asa/sample/HelloController.java
@@ -0,0 +1,30 @@
+package com.azure.asa.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<h1>Greetings from Spring Boot!</h1>");
+
+        return sb.toString();
+    }
+
+    private List<byte[]> memory = new ArrayList<>();
+
+    @PostMapping("/memory/add")
+    public String increaseMemory() {
+        byte[] bytes = new byte[32 * 1024 * 1024];
+        memory.add(bytes);
+        return "Current length is " + memory.size();
+    }
+}
---
+++ java-17-sample/src/main/java/com/azure/asa/sample/Java17SampleApplication.java
@@ -0,0 +1,13 @@
+package com.azure.asa.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java17SampleApplication.class, args);
+    }
+
+}
